2caefa7642a613af2af53b266a036ad686d9e542
Anas Abuobaid <abuobaid.anas1999@gmail.com>
2022-06-19 14:30:00 +0300

    auth

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # auth-api
+
+## UML Digrame
+
+![uml](./dev/Screenshot%20(107).png)
+
+## Links
+
+- [Heroku App](https://anas-basic-auth.herokuapp.com/)
+
+- [Pull Requset](https://github.com/abuobaid9/basic-auth/pulls?q=is%3Apr+is%3Aclosed)
+
+- [Actions](https://github.com/abuobaid9/basic-auth/actions)
---
+++ src/models/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const userModel = require('./users.js');
+const { Sequelize, DataTypes } = require('sequelize');
+const clothesModel = require('./clothes/model.js');
+const foodModel = require('./food/model.js');
+const POSTGRES_URI = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL;
+
+let sequelizeOptions =
+  process.env.NODE_ENV === "production"
+    ?
+    {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    } : {};
+
+const sequelize = new Sequelize(POSTGRES_URI, sequelizeOptions);
+const food = foodModel(sequelize, DataTypes);
+const clothes = clothesModel(sequelize, DataTypes);
+const Collection = require('./data-collection.js');
+
+module.exports = {
+  db: sequelize,
+  users: userModel(sequelize, DataTypes),
+  food: new Collection(food),
+  clothes: new Collection(clothes)
+}
